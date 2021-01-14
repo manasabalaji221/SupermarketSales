d3b79751dca5028c782e47a3538e424d90301667
manasa <manasa.balaji@mavs.uta.edu>
2021-01-14 02:21:24 -0600

    select sales

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-from flask import Flask
+from flask import Flask,render_template,request,flash
+import pyodbc
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>Hello</h1>"
+# @app.route("/")
+# def index():
+server = 'mbsalesdb.database.windows.net'
+database = 'mbsalesdb'
+username = 'salesdb'
+password = 'Manasa15'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+with pyodbc.connect(
+        'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT TOP 3 Total FROM supermarket_sales")
+        row = cursor.fetchone()
+        while row:
+            print(str(row[0]))
+            row = cursor.fetchone()
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/index', methods=['GET'])
+def date_s():
+    start_date = float(request.args['sdate'])
+    end_date = float(request.args['edate'])
+    result = []
+    c = 0
+    select="select * from supermarket_sales where " + Date + " between ? and ?"
+    # row = cursor.fetchone()
+    cur = conn.cursor()
+    cur.execute(select, (start_date, end_date))
+    rows = cur.fetchall()
+    result.append(rows)
+    print(result)
+
+
+   
+
