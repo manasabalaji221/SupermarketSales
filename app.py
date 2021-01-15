d32a0f9fe33ffa0fb6eb1cafe944658de0e84e00
manasa <manasa.balaji@mavs.uta.edu>
2021-01-15 00:07:05 -0600

    Insert data

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template,request,flash
 import pyodbc
+from random import randint
+
 app = Flask(__name__)
 
 # @app.route("/")
@@ -43,8 +45,35 @@ def date_s():
 
     return render_template('date_range.html', rows=rows, date1=start_date)
 
+@app.route('/insert1', methods=['GET'])
+def insert1():
+    branch = request.args['branch']
+    city = request.args['city']
+    ctype = request.args['ctype']
+    gender = request.args['gender']
+    prod = request.args['prod']
+    unit = request.args['unit']
+    quantity = request.args['quantity']
+    date = request.args['date']
+    time = request.args['time']
+    pay = request.args['pay']
+    rating = request.args['rating']
+
+    tax = float(unit)*float(quantity)*0.05
+    total = (float(unit)*float(quantity))+tax
+    cogs = float(total) - float(tax)
+    gmp = 4.761904762
+    gi = tax
+    n = 9
+    id1 = ''.join(["{}".format(randint(0, 9)) for num in range(0, n)])
+    id = id1[0:3] + "-" + id1[3:5] + "-" +id1[5:9]
+    insert1 = "insert into supermarket_sales ( [Invoice ID], Branch, City, [Customer type], Gender, [Product line], [Unit price], Quantity, [Tax 5%], Total, Date, Time, Payment, cogs, [gross margin percentage], [gross income], Rating ) values ( ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,? )"
+    cur = conn.cursor()
+    cur.execute(insert1, (id, branch, city, ctype, gender, prod, unit, quantity, tax, total, date, time, pay, cogs, gmp, gi, rating))
+    return render_template('insert_data.html')
+
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    # app.run(debug=True)
